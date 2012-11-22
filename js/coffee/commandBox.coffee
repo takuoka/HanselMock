1c478e1f4e43d13ebda2528ce7ff44b23473d0d3
Takuya.Okamoto <blackn.red42@gmail.com>
2012-11-22 19:50:22 +0900

    最低限のモック完成ｗ

--- js/coffee/commandBox.coffee
+++ js/coffee/commandBox.coffee
@@ -4,16 +4,3 @@ to COMPILE
 at HanselMock/
 ###
 
-
-set_page_background_color = ->
-  colors = ["red", "yellow", "white", "green"]
-  pages = document.getElementsByClassName("page")
-  i = 0
-  while i < pages.length
-    pages[i].style.backgroundColor = colors[i % 4]
-    pages[i].style.opacity = 0.6
-    i++
-
-
-add = (a=1,b=2) ->
-	return a+b+333
--- js/commandBox.js
+++ js/commandBox.js
@@ -5,28 +5,4 @@ to COMPILE
 at HanselMock/
 */
 
-var add, set_page_background_color;
 
-set_page_background_color = function() {
-  var colors, i, pages, _results;
-  colors = ["red", "yellow", "white", "green"];
-  pages = document.getElementsByClassName("page");
-  i = 0;
-  _results = [];
-  while (i < pages.length) {
-    pages[i].style.backgroundColor = colors[i % 4];
-    pages[i].style.opacity = 0.6;
-    _results.push(i++);
-  }
-  return _results;
-};
-
-add = function(a, b) {
-  if (a == null) {
-    a = 1;
-  }
-  if (b == null) {
-    b = 2;
-  }
-  return a + b + 333;
-};
